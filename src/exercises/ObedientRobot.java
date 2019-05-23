51eddcbfad2d43da9454446bd3d39ace8f33b285
Barezi79 <branislavnesic79@gmail.com>
2019-05-23 12:22:10 +0200

    Exercise 3

--- src/exercises/ObedientRobot.java
+++ src/exercises/ObedientRobot.java
@@ -2,7 +2,35 @@ package exercises;
 
 import javax.swing.JOptionPane;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class ObedientRobot {
+
+	Robot b = new Robot("mini");
+
+	private void drawCircle(int radius) {
+
+		for (int i = 0; i < 360; i++) {
+			b.move((int) ((2 * radius * Math.PI) / 360));
+			b.turn(1);
+		}
+	}
+
+	private void drawSquare(int square) {
+
+		for (int i = 0; i < 4; i++) {
+			b.move(square);
+			b.turn(90);
+		}
+	}
+
+	private void drawTriangle(int triangle) {
+
+		for (int i = 0; i < 3; i++) {
+			b.move(triangle);
+			b.turn(120);
+		}
+	}
 
 	public static void main(String[] args) {
 		int shape = JOptionPane.showOptionDialog(null, "Which shape would you like me to draw?", "Obedient Robot", 0,
@@ -11,10 +39,26 @@ public class ObedientRobot {
 		 * Create methods to draw a circle, square and triangle and call the appropriate
 		 * one depending on the user choice.
 		 */
-		
-		
+		ObedientRobot a = new ObedientRobot();
+		a.b.penDown();
+		a.b.setPenWidth(3);
+		a.b.setSpeed(15);
+		a.b.setPos(350,300);
+
+		if (shape == 0) {
+			String radiusAsString = JOptionPane.showInputDialog("Please enter the radius of a circle(min 60).");
+			int radius = Integer.parseInt(radiusAsString);
+			a.drawCircle(radius);
+		} else if (shape == 1) {
+			String squareAsString = JOptionPane.showInputDialog("Please enter the side of a square.");
+			int square = Integer.parseInt(squareAsString);
+			a.drawSquare(square);
+		} else {
+			String triangle = JOptionPane.showInputDialog("Please enter thw side of triangle.");
+			int trinagle = Integer.parseInt(triangle);
+			a.drawTriangle(trinagle);
+		}
+
 	}
-	
-	
 
 }
